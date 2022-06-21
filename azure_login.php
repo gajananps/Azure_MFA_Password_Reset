4232921a5536851dfb6b7838152886a80c8628f8
gajananps <gajananps@yahoo.com>
2022-06-21 16:59:30 +0530

    Add files via upload

---
+++ azure_login.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<title>Azure IAM Dashboard - Azure Login</title>
+
+		<!-- Google font -->
+		<link href="https://fonts.googleapis.com/css?family=Roboto&family:400,500,700" rel="stylesheet">
+
+		<!-- Bootstrap -->
+		<link type="text/css" rel="stylesheet" href="src/css/bootstrap.min.css"/>
+
+		<!-- Font Awesome Icon -->
+		<link rel="stylesheet" href="src/css/font-awesome.min.css">
+
+		<!-- Custom stlylesheet -->
+		<link type="text/css" rel="stylesheet" href="src/css/style.css"/>
+
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+    </head>
+	
+	<body>
+		<!-- HEADER -->
+		<header>
+		
+
+			<!-- MAIN HEADER -->
+			<div id="header">
+				<!-- container -->
+				<div class="container">
+					<!-- row -->
+					<div class="row">
+						<!-- LOGO -->
+						<div class="">
+							<div class="header-logo">
+								<a href="index.html" class="logo">
+									<h1>Azure IAM Dashboard - Azure Login</h1>
+								</a>
+							</div>
+						</div>
+						<!-- /LOGO -->
+
+							</div>
+						</div>
+						<!-- /ACCOUNT -->
+					</div>
+					<!-- row -->
+				</div>
+				<!-- container -->
+			</div>
+			<!-- /MAIN HEADER -->
+		</header>
+		<!-- /HEADER -->
+
+<?php
+	header("refresh: 10;");
+    $out=shell_exec('C:/Apache24/htdocs/azure-dashboard/scripts/azure_login.bat');
+    echo "<pre>$out</pre>";
+?>
+<p></p>
+<p></p>
+<p></p>
+<p>Please open URL in new tab - <a class="az-login" href="https://microsoft.com/devicelogin" target="_blank">https://microsoft.com/devicelogin</a></strong></p>
+<p>Copy the 9 digit Azure Authentication code in Captical Letters. [ Example Code.<b>IUFUD9Z2H</b>]</p>
+<p>Please enter the Authendication Code in Azure website.</p>
+
+<p></p>
+<p></p>
+<p><a href="index.html" class="cta-btn"><i class="fa fa-arrow-circle-left"></i> Back to Home</a></p>
+
+
+
+		<!-- FOOTER -->
+		<footer id="footer">
+
+			<!-- bottom footer -->
+			<div id="bottom-footer" class="section">
+				<div class="container">
+					<!-- row -->
+					<div class="row">
+						<div class="col-md-12 text-center">
+							<span class="copyright">
+								Copyright &copy;<script>document.write(new Date().getFullYear());</script> Powered By Scripting Team
+							</span>
+						</div>
+					</div>
+						<!-- /row -->
+				</div>
+				<!-- /container -->
+			</div>
+			<!-- /bottom footer -->
+		</footer>
+		<!-- /FOOTER -->
+
+		<!-- jQuery Plugins -->
+		<script src="js/jquery.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="js/slick.min.js"></script>
+		<script src="js/nouislider.min.js"></script>
+		<script src="js/jquery.zoom.min.js"></script>
+		<script src="js/main.js"></script>
+
+	</body>
+</html>
---
+++ csv_upload_db.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<title>Azure IAM Dashboard</title>
+
+		<!-- Google font -->
+		<link href="https://fonts.googleapis.com/css?family=Roboto&family:400,500,700" rel="stylesheet">
+
+		<!-- Bootstrap -->
+		<link type="text/css" rel="stylesheet" href="src/css/bootstrap.min.css"/>
+
+		<!-- Font Awesome Icon -->
+		<link rel="stylesheet" href="src/css/font-awesome.min.css">
+
+		<!-- Custom stlylesheet -->
+		<link type="text/css" rel="stylesheet" href="src/css/style.css"/>
+
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+    </head>
+	
+	<body>
+		<!-- HEADER -->
+		<header>
+		
+
+			<!-- MAIN HEADER -->
+			<div id="header">
+				<!-- container -->
+				<div class="container">
+					<!-- row -->
+					<div class="row">
+						<!-- LOGO -->
+						<div class="">
+							<div class="header-logo">
+								<a href="index.html" class="logo">
+									<h1>Azure IAM Dashboard</h1>
+								</a>
+							</div>
+						</div>
+						<!-- /LOGO -->
+
+							</div>
+						</div>
+						<!-- /ACCOUNT -->
+					</div>
+					<!-- row -->
+				</div>
+				<!-- container -->
+			</div>
+			<!-- /MAIN HEADER -->
+		</header>
+		<!-- /HEADER -->
+		
+		
+		<!-- SECTION -->
+		<div class="section">
+			<!-- container -->
+			<div class="container">
+				<!-- row -->
+				<div class="row">
+					
+					
+					<!-- DB Query and CSV File  import - Start  -->
+					<div class="col-md-4">
+						
+						<p>&nbsp;</p>
+						<p>&nbsp;</p>
+						<h3><a href="http://localhost/azure-dashboard/scripts/mysql_query_execute.bat" class="cta-btn"><img src="src/img/download.png" alt=""> Download Database Script</a></h3>
+						<p>&nbsp;</p>
+						
+						<p>Please save file to C:\ in your local Machine</p>
+						<p></p>
+						<p>Open Windows Command Prompt</p>
+						<p>Execute manually below command once</p>
+						<p>C:\mysql_query_execute.bat</p>
+						<p></p>
+						<p></p>
+						
+						
+						
+					</div>
+					<!-- DB Query and CSV File  import - End  -->
+					
+					
+					
+					
+				</div>
+				<!-- /row -->
+			</div>
+			<!-- /container -->
+		</div>
+		<!-- /SECTION -->
+
+
+
+
+
+
+<p><a href="csv_mysql_import.html" class="cta-btn"><i class="fa fa-arrow-circle-left"></i> Back to Home</a></p>
+
+
+
+		<!-- FOOTER -->
+		<footer id="footer">
+
+			<!-- bottom footer -->
+			<div id="bottom-footer" class="section">
+				<div class="container">
+					<!-- row -->
+					<div class="row">
+						<div class="col-md-12 text-center">
+							<span class="copyright">
+								Copyright &copy;<script>document.write(new Date().getFullYear());</script> Powered By Scripting Team
+							</span>
+						</div>
+					</div>
+						<!-- /row -->
+				</div>
+				<!-- /container -->
+			</div>
+			<!-- /bottom footer -->
+		</footer>
+		<!-- /FOOTER -->
+
+		<!-- jQuery Plugins -->
+		<script src="js/jquery.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="js/slick.min.js"></script>
+		<script src="js/nouislider.min.js"></script>
+		<script src="js/jquery.zoom.min.js"></script>
+		<script src="js/main.js"></script>
+
+	</body>
+</html> 
